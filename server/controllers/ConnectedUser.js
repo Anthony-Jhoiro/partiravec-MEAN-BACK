49332f1e79e2aa688c1a7746ffed606f31d02a6d
Anthony <anthony.quere@lacatholille.fr>
2020-06-29 13:47:02 +0200

    try to fix it

--- server/controllers/ConnectedUser.js
+++ server/controllers/ConnectedUser.js
@@ -16,55 +16,63 @@ const Inbox = require('../models/Inbox');
 const Group = require('../models/Group');
 
 class ConnectedUser {
-  userInfos;
-  socket;
+    userInfos;
+    socket;
 
-  constructor(socket) {
-    this.socket = socket;
-    this.userInfos = {socketId: socket.id, userId: null};
-  }
+    constructor(socket) {
+        this.socket = socket;
+        this.userInfos = {socketId: socket.id, userId: null};
+    }
 
-  identify(data, next) {
-    const token = data.token;
-    jwtVerify(token,
-      decoded => {
-        this.userInfos.userId = decoded.id;
-        Group.find({contributors: this.userInfos.userId})
-          .then(groups => {
-            groups.forEach(g => {
-              this.socket.join(g._id);
+    identify(data, next) {
+        const token = data.token;
+        jwtVerify(token,
+            decoded => {
+                this.userInfos.userId = decoded.id;
+                Group.find({contributors: this.userInfos.userId})
+                    .then(groups => {
+                        groups.forEach(g => {
+                            this.socket.join(g._id);
+                        });
+                        this.socket.on('message', (data, next) => this.onMessage(data, next));
+                        next();
+                    });
+
+            },
+            () => {
+                try {
+                    this.socket.emit('err', 'La connexion a échoué')
+                } catch (e) {
+                    console.error(e);
+                }
+
             });
-            this.socket.on('message', (data, next) => this.onMessage(data, next));
-            next();
-          });
 
-      },
-      () => this.socket.emit('err', 'La connexion a échoué'));
-  }
+    }
 
-  onMessage(data, next) {
-    // user has access to room
-    Group.findOne({_id: data.room})
-      .then(group => {
+    onMessage(data, next) {
+        // user has access to room
+        Group.findOne({_id: data.room})
+            .then(group => {
 
-        if (! group)
-          return this.socket.emit('error', "La room n'existe pas.");
-        if (! group.hasAccess(this.userInfos.userId))
-          return this.socket.emit('error', "Vous n'êtes pas authorisé à écrire ici");
+                if (!group)
+                    return this.socket.emit('error', "La room n'existe pas.");
+                if (!group.hasAccess(this.userInfos.userId))
+                    return this.socket.emit('error', "Vous n'êtes pas authorisé à écrire ici");
 
-        const message = new Inbox({
-          from: this.userInfos.userId,
-          to: group.id,
-          body: data.message
-        });
+                const message = new Inbox({
+                    from: this.userInfos.userId,
+                    to: group.id,
+                    body: data.message
+                });
 
-        message.save((err, message) => {
-          if (err) return this.socket.emit('error', "Impossible d'envoyer le message");
-          this.socket.broadcast.to(data.room).emit('message', message);
-          next(message);
-        })
-      });
-  }
+                message.save((err, message) => {
+                    if (err) return this.socket.emit('error', "Impossible d'envoyer le message");
+                    this.socket.broadcast.to(data.room).emit('message', message);
+                    next(message);
+                })
+            });
+    }
 }
 
 module.exports = ConnectedUser;
