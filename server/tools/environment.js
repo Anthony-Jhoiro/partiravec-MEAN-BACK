751f858553fef54b96cf164b15a7a01acdbca76a
Anthony <anthony.quere@lacatholille.fr>
2020-06-30 17:15:16 +0200

    update default env

--- server/tools/environment.js
+++ server/tools/environment.js
@@ -18,13 +18,13 @@ if (process.env.ENVIRONMENT !== 'prod') {
 module.exports = {
   PORT: process.env.PORT,
   ENVIRONMENT: process.env.ENVIRONMENT | 'dev',
-  JWT_SECRET: process.env.JWT_SECRET,
-  MONGODB_URI: process.env.MONGODB_URI,
+  JWT_SECRET: process.env.JWT_SECRET | "abracadabra",
+  MONGODB_URI: process.env.MONGODB_URI | "mongodb://127.0.0.1/partiravec",
   UPLOAD_FOLDER: process.env.PWD + '/upload/',
   ENDPOINT: process.env.ENDPOINT,
   GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
   FRONT_URL: process.env.FRONT_URL,
   AMAZON_ACCESS_KEY: process.env.AMAZON_ACCESS_KEY,
   AMAZON_SECRET_ACCESS_KEY: process.env.AMAZON_SECRET_ACCESS_KEY,
-  IMAGE_STORAGE_MODE: process.env.IMAGE_STORAGE_MODE,
+  IMAGE_STORAGE_MODE: process.env.IMAGE_STORAGE_MODE | "local",
 }
